d413269b205fe57be13b545e62def87ccd19d2f1
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-06-07 10:35:49 +0200

    Give dynamic class to button with props

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -14,7 +14,7 @@
 
         <div class="ml-auto flex h-full items-center">
           <ProfileImage v-if="isLoggedIn" />
-          <ActionButton v-else @click="loginUser" />
+          <ActionButton v-else text="Sign In" :is-primary="false" @click="loginUser" />
         </div>
       </div>
     </div>
