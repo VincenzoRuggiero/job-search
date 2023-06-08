bd0ef848bbc3251e4dfe1da758793e6d5b04ae7b
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-06-08 11:07:14 +0200

    Styling action word with hooks

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,6 +1,10 @@
 <template>
   <section>
-    <h1>{{ action }} for everyone</h1>
+    <h1>
+      <span :class="actionClasses">{{ action }}</span
+      ><br />
+      for everyone
+    </h1>
     <h2>Find your next job at Jobbing</h2>
   </section>
 </template>
@@ -14,14 +18,27 @@ export default {
       interval: null
     }
   },
+  computed: {
+    actionClasses() {
+      // Applies the right class color based on data
+      return {
+        build: this.action === 'Build',
+        create: this.action === 'Create',
+        design: this.action === 'Design',
+        code: this.action === 'Code'
+      }
+    }
+  },
   created() {
     this.changeTitle()
   },
   beforeUnmount() {
+    // Cancels action loop
     clearInterval(this.interval)
   },
   methods: {
     changeTitle() {
+      // Loops through actions everye 3 secs and updates data
       this.interval = setInterval(() => {
         const actions = ['Build', 'Create', 'Design', 'Code']
         const currentActionIndex = actions.indexOf(this.action)
@@ -33,3 +50,21 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+
+.create {
+  color: #34a853;
+}
+
+.design {
+  color: #f9ab00;
+}
+
+.code {
+  color: #d93025;
+}
+</style>
